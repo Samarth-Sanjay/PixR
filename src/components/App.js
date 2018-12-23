8085015861c9f613c2634c1643d95271326d786c
Samarth <samarth.sanjay@ymail.com>
2018-12-23 16:17:33 +0530

    added unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
 
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+class App extends React.Component {
+    state = { images: [] };
+
+     onSearchSubmit = async (term) =>   {
+        const response = await axios.get('https://api.unsplash.com/search/photos',
+            {
+                params: { query : term,
+                page : 1},
+                headers: {
+                    Authorization : "Client-ID d4994ef03adb8936af4a3078580c7e37566d03408e933e9d594768532873023a"
+                }
+
+            });
+         console.log(response.data.results);
+       // this.setState({images: response.data.results});
+
+    }
+
+    render() {
+        return (
+
+            <div className= "ui container" style={{marginTop: '15px'}}>
+                <SearchBar onSubmitting={this.onSearchSubmit} />
+                found {this.state.images};
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
+
+
+class SearchBar extends React.Component {
+
+    state = {term: ''};
+
+  onSubmitChange= (event) => {
+      event.preventDefault();
+      this.props.onSubmitting(this.state.term);
+  };
+
+
+
+
+
     render() {
         return (
-            <div>
-                <form>
-                    <input/>
+            <div className="ui segment">
+                {/*<form onSubmit={e => {e.preventDefault(); console.log(this.state.term)}} className="ui form">*/}
+                    <form onSubmit={this.onSubmitChange} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        {/*<input type="text" onChange={this.onInputChange}  placeholder={"Search..."}/>*/}
+                        <input type="text" value={this.state.term} onChange={ e => this.setState({term: e.target.value})}  />
+                    </div>
                 </form>
             </div>
         );
