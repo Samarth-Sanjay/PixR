18b7e6f7ca0b85d230eac4371fe7b612f7be2fa7
Samarth <samarth.sanjay@ymail.com>
2018-12-23 22:27:53 +0530

    Updates

---
+++ precache-manifest.bbe5603eb2bd726b170e82a8f8059f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f113411122800ecca81a",
+    "url": "/PixR/static/js/runtime~main.f1134111.js"
+  },
+  {
+    "revision": "71598cd2db8a5498e182",
+    "url": "/PixR/static/js/main.71598cd2.chunk.js"
+  },
+  {
+    "revision": "f7d26e3cd4246738ab5a",
+    "url": "/PixR/static/js/1.f7d26e3c.chunk.js"
+  },
+  {
+    "revision": "71598cd2db8a5498e182",
+    "url": "/PixR/static/css/main.8cce4ad7.chunk.css"
+  },
+  {
+    "revision": "58bb577295ac1adda625380c855be84d",
+    "url": "/PixR/index.html"
+  }
+];
